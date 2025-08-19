4aee727ccbc0969d052f28b8e645b3d18fed69d8
noONEdare <tnitinsingh40@gmail.com>
2025-08-19 18:16:34 +0530

    Update bubble_sort.java

--- bubble_sort.java
+++ bubble_sort.java
@@ -2,17 +2,21 @@ class bubbleSorting{
     public static void printarr(int[] arr){
         int size = arr.length;
         for(int i=0; i<size; i++){
-            System.out.print(arr[i]);
+            System.out.print(arr[i]+" ");
         }
+        System.out.println();
     }
-    public static void main(string[] args){
+    public static void main(String[] args){
         int[] arr = {5, 6, 4, 2, 3, 8, 1, 10, 9};
-         //buble sort 
-        for(int i=0; i<arr.length; i++){
-            for(int j=1; i<=arr.length-i-1; i++){
-                int temp = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = temp;
+        printarr(arr);
+        //bubble sort
+        for(int i=0; i<arr.length-1; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
             }
         }
         printarr(arr);
