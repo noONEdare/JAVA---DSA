a7c354979ad6c3e45c021251a53f27ac7ffd38aa
NITIN SINGH THAKUR <tnitinsingh40@gmail.com>
2025-08-22 14:25:24 +0530

    sorting

---
+++ bubbleSorting.java
@@ -0,0 +1,25 @@
+public class bubbleSorting {
+    public static void printarr(int[] arr) {
+        int size = arr.length;
+        for (int i = 0; i < size; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 6, 4, 2, 3, 8, 1, 10, 9 };
+        printarr(arr);
+        // bubble sort
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        printarr(arr);
+    }
+}
