0b4ae95b28f50071672f7d7fcf6d6d63dd534b97
NITIN SINGH THAKUR <tnitinsingh40@gmail.com>
2025-09-04 15:41:49 +0530

    searching

---
+++ binarySearch.java
@@ -0,0 +1,26 @@
+public class binarySearch {
+    public static int searchTarget(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 4, 5, 6, 7, 8, 10, 12, 13, 15, 18, 20 };
+        int target = 12;
+
+        int result = searchTarget(arr, target);
+
+        System.out.println("target found on index:" + result);
+    }
+}
