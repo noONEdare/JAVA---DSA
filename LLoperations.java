8eceaf083933ccbd34c7e1bb7c79b21e9831af95
NITIN SINGH THAKUR <tnitinsingh40@gmail.com>
2025-09-08 22:04:30 +0530

    linked list basics

---
+++ LLoperations.java
@@ -0,0 +1,154 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class LLoperations {
+    Node head;
+
+    // insert at head:
+    public void insertAtHead(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // insert at tail or last:
+    public void insertAtLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // insert at index:
+    public void insertAtIndex(int index, int data) {
+        if (index == 0) {
+            insertAtHead(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        for (int i = 0; temp != null && i < index - 1; i++) {
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("Index out of bound...");
+            return;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    // delete head:
+    public void deleteHead() {
+        if (head == null)
+            return;
+        head = head.next;
+    }
+
+    // delete tail:
+    public void deleteTail() {
+        if (head == null)
+            return;
+        if (head.next == null) {
+            head = null;
+            return;
+        }
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+
+    // delete at index:
+    public void deleteAtIndex(int index) {
+        if (head == null)
+            return;
+        if (index == 0) {
+            deleteHead();
+            return;
+        }
+        Node temp = head;
+        for (int i = 0; temp != null && i < index - 1; i++) {
+            temp = temp.next;
+        }
+        if (temp == null || temp.next == null) {
+            System.out.println("Inex out of bound...");
+            return;
+        }
+        temp.next = temp.next.next;
+    }
+
+    // search for a value:
+    public boolean search(int key) {
+        Node temp = head;
+        while (temp != null) {
+            if (temp.data == key)
+                return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+
+    // reverse the list:
+    public void reverse() {
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    // print list:
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        LLoperations list = new LLoperations();
+        list.insertAtHead(20);
+        list.insertAtHead(10);
+        list.insertAtLast(40);
+        list.insertAtIndex(2, 30);
+
+        list.printList();
+
+        list.reverse();
+        list.printList();
+
+        list.deleteAtIndex(2);
+        list.printList();
+
+        list.deleteHead();
+        list.printList();
+
+        list.deleteTail();
+        list.printList();
+
+        System.out.println("search for 10:" + list.search(20));
+        System.out.println("search for 50:" + list.search(50));
+    }
+}
