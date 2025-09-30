600de80e42d84ec5e33d18be3a4c018397291c16
NITIN SINGH THAKUR <tnitinsingh40@gmail.com>
2025-09-30 22:47:05 +0530

    Binarry Tree Advance

--- treeAdvance.java
+++ treeAdvance.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 class treeInfo {
     int height;
     int diameter;
@@ -75,10 +78,67 @@ public class treeAdvance {
         return new treeInfo(myHeight, myDiameter);
     }
 
+    public boolean isIdentical(Tnode root, Tnode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null || subRoot == null) {
+            return false;
+        }
+        if (root.data == subRoot.data) {
+            return isIdentical(root.left, subRoot.left) && isIdentical(root.right, subRoot.right);
+        }
+        return false;
+    }
+
+    public boolean isSubTree(Tnode root, Tnode subRoot) {
+        if (subRoot == null) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+
+        if (isIdentical(root, subRoot))
+            return true;
+
+        return isSubTree(root.left, subRoot) || isSubTree(root.right, subRoot);
+    }
+
+    public int sumAtLvlK(int k, Tnode root) {
+        if (root == null)
+            return 0;
+
+        Queue<Tnode> q = new LinkedList<>();
+        q.add(root);
+        int level = 0;
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int sum = 0;
+            for (int i = 0; i < size; i++) {
+                Tnode curr = q.poll();
+
+                if (level == k) {
+                    sum += curr.data;
+                }
+
+                if (curr.left != null)
+                    q.add(curr.left);
+                if (curr.right != null)
+                    q.add(curr.right);
+            }
+            if (level == k)
+                return sum;
+
+            level++;
+        }
+        return 0;
+    }
+
     public static void main(String[] args) {
         int[] nodes = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
         Tnode root = treeAdvance.buildTree(nodes);
-        System.err.println("Root Node: " + root.data);
+        System.out.println("Root Node: " + root.data);
 
         treeAdvance tree = new treeAdvance();
         int count = tree.countNodes(root);
@@ -96,5 +156,15 @@ public class treeAdvance {
         int diameter2 = tree.diameterOfTree1(root);
         System.out.println("Diameter of Tree(O(n)): " + diameter2);
 
+        idx = -1;
+        int[] subNodes = { 2, 4, -1, -1, 5, -1, -1 };
+        Tnode subRoot = treeAdvance.buildTree(subNodes);
+        System.out.println("SubRoot Node: " + subRoot.data);
+
+        boolean subTree = tree.isSubTree(root, subRoot);
+        System.out.println("Sub of Tree: " + subTree);
+
+        int sumAtLvl = tree.sumAtLvlK(2, root);
+        System.out.println("Sub of nodes at level K: " + sumAtLvl);
     }
 }
